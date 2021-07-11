6ec71a6556566833346c9d855dbbeada7c148413
ritikpy <ritikvarshney2014@gmail.com>
2021-07-11 11:57:55 +0530

    --added url validation

--- server/controllers/urlController.js
+++ server/controllers/urlController.js
@@ -1,31 +1,34 @@
 const Url = require('../models/Url');
 const shortId = require("shortid");
+const utils = require('../utils/utils');
 require('dotenv').config();
 
 const shortUrl = async (req,res)=> {
     const { originalUrl } = req.body;
     const base = process.env.BASE;
     const urlId = shortId.generate();
-    try{
-        let url = await Url.findOne({ originalUrl });
-        if(url){
-            return res.status(200).json(url);
-        } else {
-            const shortUrl = `${base}/${urlId}`;
-            url = new Url({
-                urlId,
-                originalUrl,
-                shortUrl,
-                date: new Date()
-            });
-            await url.save();
-            return res.status(200).json(url);
+    if(utils.validateUrl(originalUrl)){
+        try{
+            let url = await Url.findOne({ originalUrl });
+            if(url){
+                return res.status(200).json(url);
+            } else {
+                const shortUrl = `${base}/${urlId}`;
+                url = new Url({
+                    urlId,
+                    originalUrl,
+                    shortUrl,
+                    date: new Date()
+                });
+                await url.save();
+                return res.status(200).json(url);
+            }
+        } catch(e){
+            return res.status(500).json(e);
         }
-    } catch(e){
-        return res.status(500).json(e);
+    }else{
+        return res.status(400).json({message: 'Invalid Url'});
     }
-    
-    
 }
 
 const fetchUrl = async (req,res)=> {
